34dc727a4efcd7aeb7d340ac00a449e2f45b6cfb
Antoine Lauzis <antoine.lauzis@etudiant.univ-reims.fr>
2024-03-06 12:24:21 +0100

    feat : pockedex working

---
+++ views/Pokedex.js
@@ -0,0 +1,83 @@
+import { SafeAreaView, StyleSheet, View, StatusBar, Text, ScrollView, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState, useCallback } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+
+import PokeCard from './components/PokeCard';
+
+import * as color from '../styles/variables/colors';
+import * as text from '../styles/texts';
+import * as layout from '../styles/layouts';
+
+export default function Pokedex() {
+    const [data, setData] = useState([]);
+
+    const pokedexIds = async () => {
+        const value = await AsyncStorage.getItem('pokedex');
+        if (value !== null) {
+            const pokemonIds = JSON.parse(value);
+            console.log(pokemonIds);
+            loadData(pokemonIds);
+        }
+    };
+
+    const loadData = async (pokemonIds) => {
+        const pokemonData = await Promise.all(pokemonIds.map(async (id) => {
+            const apiLink = `https://pokeapi.co/api/v2/pokemon/${id}`;
+            try {
+                const response = await axios.get(apiLink);
+                return response.data;
+            } catch (error) {
+                console.error(error);
+                return null;
+            }
+        }));
+
+        setData(pokemonData.filter(Boolean));
+    };
+
+    useFocusEffect(
+        useCallback(() => {
+            pokedexIds();
+        }, [])
+    );
+
+    return (
+        <SafeAreaView style={StyleSheet.compose(
+            layout.container, {
+              backgroundColor: color.gray,
+            }
+          )}
+        >
+            <StatusBar style="auto" />
+            <View style={layout.container}>
+                <View style={layout.titleContainer}>
+                    <Text style={text.mainTitle}>Pokedex</Text>
+                </View>
+                <ScrollView>
+                    <TouchableOpacity 
+                        style={StyleSheet.compose(
+                            layout.container, {
+                                display: 'flex',
+                                flexDirection: 'row',
+                                flexWrap: 'wrap',
+                                justifyContent: 'space-evenly',
+                            }
+                        )}
+                        
+                    >
+                        {data.map((item, index) => (
+                            <PokeCard
+                                key={index}
+                                id={item.id}
+                                name={item.name}
+                                imageUrl={item.sprites.front_default}
+                            />
+                        ))}
+                    </TouchableOpacity>
+                </ScrollView>
+            </View>
+        </SafeAreaView>
+    );
+}
